51ca5b46de240f346fedfe3c4226a1ca9984b467
Matheus Ribeiro <srdmatheus@gmail.com>
2024-08-01 13:41:58 -0300

    feat: add fonts & global colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,36 @@
 import type { Config } from "tailwindcss";
+import { fontFamily } from "tailwindcss/defaultTheme";
 
 const config: Config = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        border: "rba(var(--border))",
+        background: {
+          DEFAULT: "rgb(var(--background))",
+          foreground: "rgb(var(--background-foreground))"
+        },
+        foreground: {
+          DEFAULT: "rgb(var(--foreground))"
+        },
+        danger: {
+          DEFAULT: "rgb(var(--danger))"
+        },
+        success: {
+          DEFAULT: "rgb(var(--success))"
+        },
+        primary: {
+          DEFAULT: "rgb(var(--primary))"
+        }
+      },
+      boxShadow: {
+        main: "4px 4px 0 rgb(var(--shadow))"
+      },
+      fontFamily: {
+        sans: ["var(--font-rubik)", ...fontFamily.sans]
+      }
+    }
   },
   plugins: []
 };
