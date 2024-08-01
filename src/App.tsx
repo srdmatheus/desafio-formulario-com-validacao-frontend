e584b2973bbd43525c7c27f6970d2a866135b66e
Matheus Ribeiro <srdmatheus@gmail.com>
2024-08-01 13:45:30 -0300

    feat: add register form to home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { RegisterForm } from "./components/register-form";
+
 export const App = () => {
   return (
-    <main>
-      <h1>hello world</h1>
+    <main className="font-rubik flex h-dvh w-full flex-col items-center justify-center bg-zinc-50 px-4">
+      <h1 className="text-2xl font-bold tracking-tight text-foreground md:text-3xl">
+        Formulário de cadastro
+      </h1>
+      <div className="w-80 md:w-[400px]">
+        <RegisterForm />
+      </div>
     </main>
   );
 };
